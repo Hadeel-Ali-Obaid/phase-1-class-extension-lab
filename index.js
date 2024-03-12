0dc819441eaf9ef1ea943bb8c1e9286dc0fd9e4f
Hadeel Obaid <hadeelobaid71@gmail.com>
2024-03-12 17:39:43 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Your code here
+class Polygon {
+  constructor(Array) {
+    this.Array = Array;
+  }
+  get countSides() {
+    return this.Array.length;
+  }
+  get perimeter() {
+    let sum = 0;
+    for (let i = 0; i < this.Array.length; i++) {
+      sum = sum + this.Array[i];
+    }
+    return sum;
+  }
+}
+
+class Triangle extends Polygon {
+  get isValid() {
+    if (
+      this.Array[0] + this.Array[1] > this.Array[1] &&
+      this.Array[0] + this.Array[2] > this.Array[2] &&
+      this.Array[1] + this.Array[2] > this.Array[0]
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+class Square extends Polygon {
+  get isValid() {
+    if (
+      this.Array[0] == this.Array[1] &&
+      this.Array[0] == this.Array[2] &&
+      this.Array[0] == this.Array[3]
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  get area() {
+    return this.Array[0] * this.Array[0];
+  }
+}
